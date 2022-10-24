f31a0fcfaebedb7a195337cfe483dcae9260c481
waylenwasywas <35209231+waylenwasywas@users.noreply.github.com>
2022-10-24 15:14:53 -0600

    Update Client.sh

--- Client.sh
+++ Client.sh
@@ -4,4 +4,7 @@ apt install resolvconf
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& random.trust_cpu=off/' /etc/default/grub
 sed -i 's/GRUB_CMDLINE_LINUX="[^"]*/& random.trust_cpu=off/' /etc/default/grub
 sysctl -w net.ipv4.ip_forward=1
+sysctl -p
+echo "this system will now reboot to save changes"
+reboot
 update-grub
