24b6a37e79ba13d18f6b7a6678d18c090606811f
waylenwasywas <35209231+waylenwasywas@users.noreply.github.com>
2022-10-24 14:35:44 -0600

    Update Client.sh

--- Client.sh
+++ Client.sh
@@ -1,5 +1,6 @@
 apt update
 apt install wireguard
+apt install resolvconf
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& random.trust_cpu=off/' /etc/default/grub
 sed -i 's/GRUB_CMDLINE_LINUX="[^"]*/& random.trust_cpu=off/' /etc/default/grub
 sysctl -w net.ipv4.ip_forward=1
